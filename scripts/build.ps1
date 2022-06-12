b9ba9bc19225127b68d8c01db876faf4f07a3df4
KuteKetX <KuteKetBhop@gmail.com>
2022-06-12 10:00:36 -0600

    Improved build script to add extra steps needed in the build process

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,7 +1,24 @@
+function Get-BuiltWheel {
+    Get-ChildItem -Path dist -Filter *.whl | Foreach-Object {
+        return $_.FullName
+    }
+}
+
+
 if (Test-Path -Path dist) {
+    Write-Host "Removing previous builds from the dist/ directory..." -ForegroundColor Magenta
     Remove-Item -Path dist -Recurse
 }
 
 Write-Host "Building project wheels..." -ForegroundColor Magenta
+
 # NOTE: The -n flag is here because virtualenv was freaking out preventing me from building.
 py -m build -n
+
+Write-Host "Removing previous build from packages..." -ForegroundColor Magenta
+py -m pip uninstall pyntfy
+
+Write-Host "Installing built package..." -ForegroundColor Magenta
+
+$whl_file = Get-BuiltWheel
+py -m pip install $whl_file
